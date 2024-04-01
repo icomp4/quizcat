d7e03a66887794ee53c5f65188ebd8295590fd07
isaiah compere <rewrittenzip@gmail.com>
2024-04-01 02:20:00 -0400

    working on frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppNavbar />
+  <FilterBox />
+</template>
+
+<script>
+import AppNavbar from './components/AppNavbar.vue'
+import FilterBox from './components/FilterBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNavbar,
+    FilterBox
+  }
+}
+</script>
+
---
+++ frontend/src/components/AppNavbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar">
+    <h1 class="quizHeader">QuizCat</h1>
+    <ul class="nav-links">
+      <li>
+        <router-link to="/">Explore</router-link>
+      </li>
+      <li>
+        <router-link to="/quizzes">Create</router-link>
+      </li>
+      <li>
+        <router-link to="/create">Sign up</router-link>
+      </li>
+      <li>
+        <router-link to="/login">Login</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+
+.quizHeader {
+  margin-left: 50%;
+  transform: translateX(-50%);
+  font-weight: bold;
+}
+
+.nav-links {
+  display: flex;
+  justify-content: space-between;
+  width: 20%;
+  margin-right: 5%;
+}
+
+.nav-links li {
+  list-style: none;
+  padding: 10px;
+  color: rgb(36, 36, 36);
+}
+.nav-links li:hover {
+  color: rgb(43, 117, 179);
+  cursor: pointer;
+}
+
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+    isOpen: false
+    }
+  }
+}
+</script>
