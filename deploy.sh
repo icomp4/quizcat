6ba2be372156f4999f7a19be28c8de3397d3e0ad
isaiah compere <rewrittenzip@gmail.com>
2024-04-21 05:21:36 -0400

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,14 @@ npm run build
 # Returning to the root directory
 cd ..
 
-# Navigating to 'migrate' and executing commands
-cd migrate || exit
-cd up || exit
+# Navigating to 'migrate/up' and assuming 'up' is a directory
+cd migrate/up || exit
 
-# Assuming 'cd up' is a directory change and not a command. If 'up' is a command, adjust accordingly.
-cd .. || exit
-
-# Running a Go program, assuming 'main.go' is in the 'migrate' directory
+# Running a Go program, assuming 'main.go' is in the 'migrate/up' directory
 go run main.go
 
 # Navigating back to the root directory
-cd ../..
+cd ../../..
 
 # Running the main Go program from the root directory
 go run main.go
