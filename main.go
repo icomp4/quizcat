fb0e11fbc7e03467530d836c6f16c8fc045e8126
isaiah compere <rewrittenzip@gmail.com>
2024-04-21 04:46:33 -0400

    updated port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"os"
@@ -20,31 +21,33 @@ import (
 func main() {
 	db := database.New()
 	store := session.New(session.Config{
-		CookieSecure:  true,
+		CookieSecure:   true,
 		CookieHTTPOnly: true,
-		Expiration: 30 * (time.Hour * 24),
+		Expiration:     30 * (time.Hour * 24),
 	})
 	logger := slog.New(slog.Default().Handler())
 	quizService := service.NewQuizService(db)
 	userService := service.NewUserService(db)
-				categoryService := service.NewCategoryService(db)
-				questionService := service.NewQuestionService(db)
+	categoryService := service.NewCategoryService(db)
+	questionService := service.NewQuestionService(db)
 
-				quizHandler := handler.NewQuizHandler(*quizService, store, *logger)
-				userHandler := handler.NewUserHandler(*userService, store, *logger)
-				categoryHandler := handler.NewCategoryHandler(*categoryService, store, *logger)
-				questionHandler := handler.NewQuestionHandler(*questionService, store, *logger)
+	quizHandler := handler.NewQuizHandler(*quizService, store, *logger)
+	userHandler := handler.NewUserHandler(*userService, store, *logger)
+	categoryHandler := handler.NewCategoryHandler(*categoryService, store, *logger)
+	questionHandler := handler.NewQuestionHandler(*questionService, store, *logger)
 
-				app := server.New(fiber.New(), quizHandler, userHandler, categoryHandler, questionHandler, store)
-				app.App.Use(cors.New(cors.Config{
-					AllowOrigins:     "http://localhost:3000",
-					AllowCredentials: true,
-					AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
-					AllowHeaders:     "Content-Type, Authorization",
-				}))
-	
+	app := server.New(fiber.New(), quizHandler, userHandler, categoryHandler, questionHandler, store)
+	app.App.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Content-Type, Authorization",
+	}))
+
 	server.SetupRoutes(app)
-	log.Fatal(app.App.Listen(":" + os.Getenv("PORT")))
+	port := os.Getenv("PORT")
+	portStr := fmt.Sprintf("0.0.0.0:%s", port)
+	log.Fatal(app.App.Listen(portStr))
 	util.StartPlayCountResetScheduler(db)
 	select {}
 }
