1f378b491975861f7b1baae178681b10ab28d301
isaiah compere <rewrittenzip@gmail.com>
2024-04-21 05:35:22 -0400

    deploy and vue-cli

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Navigating to 'frontend' directory and building the project
 cd frontend || exit
 npm run build
 
-# Returning to the root directory
 cd ..
 
-# Navigating to 'migrate/up' and assuming 'up' is a directory
 cd migrate/up || exit
 
-# Running a Go program, assuming 'main.go' is in the 'migrate/up' directory
 go run main.go
 
-# Navigating back to the root directory
-cd ../../
+cd ..
+cd ..
 
-# Running the main Go program from the root directory
 go run main.go
